b542909a36fb968d4588fb4948e902d176fd4a85
harsh jain <harshjain@avinetworks.com>
2020-03-01 20:33:34 +0000

    git_fetch_cron_job

---
+++ scripts/git_fetch_cron_job.py
@@ -0,0 +1,18 @@
+import pexpect,datetime
+logs = []
+try:
+    c = pexpect.spawn('git fetch -p -P --force',cwd='/root/workspace/avi-dev')
+    c.expect("Enter passphrase for key '/root/.ssh/id_rsa':")
+    logs.append(c.after)
+    c.sendline('maddy')
+    c.expect(pexpect.EOF, timeout=120)
+    logs.append(c.before)
+except Exception as e:
+    logs.append(str(e))
+
+with open('/root/logfile_git_fetch_cron.txt','a') as ff:
+    ff.write('******** ' + str(datetime.datetime.now()) + '  ********' + '\n')
+    for log in logs:
+        ff.write(log)
+    ff.write('\n')
+# crontab -e has : 0 */8 * * * /usr/bin/python /root/scripts/git_fetch_cron_job.py
