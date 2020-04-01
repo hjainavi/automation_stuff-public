9cd7eed27e95e61e44aff9d3e158c23ed87227c9
harsh jain <harshjain@avinetworks.com>
2020-04-01 19:02:01 +0000

    sdk scripts

--- scripts/import.py
+++ scripts/import.py
@@ -1,46 +1,35 @@
-from avi.rest.view_utils import process_view_request
-from permission.models import User
-user = User.objects.get(name='admin')
+from avi.sdk.avi_api import ApiSession
+import argparse, json
 
-imp_data =  {'configuration' :{"User": [
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user', help='controller user',
+                        default='admin')
+    parser.add_argument('-p', '--password', help='controller user password',
+                        default='avi123')
+    parser.add_argument('-t', '--tenant', help='tenant name',
+                        default='admin')
+    parser.add_argument('-c', '--controller_ip', help='controller ip')
+    parser.add_argument('-v', '--version', help='controller ip')
+
+    args = parser.parse_args()
+    api = ApiSession.get_session(args.controller_ip, args.user, args.password,
+                                 tenant=args.tenant,api_version=args.version)
+    
+    imp_data =  {'configuration' :{"Pool": [
         {
-            "access": [
-                {
-                "role_ref": "/api/role/?tenant=admin&name=Application-Operator",    
-                "tenant_ref":"/api/tenant/?name=admin"
-                }
-            ],
-            "is_superuser": True,
-            "name": "token_normal_user_1",
-            "password": "token_normal_user_1",
-            "username": "token_normal_user_1"
-        },
-        {   
-            "access": [
-                {
-                "role_ref": "/api/role/?tenant=admin&name=System-Admin",
-                "tenant_ref":"/api/tenant/?name=admin"
-                }
-            ],
-            "is_superuser": True,
-            "name": "token_super_user_1",
-            "password": "token_super_user_1",
-            "username": "token_super_user_1"
+            "server":[{'ip':{'type':'V8'}}]
         }
+    ]}}
+    
+    post_data = api.post('configuration/import', data=imp_data)
+    import ipdb;ipdb.set_trace()
+    a=1
 
-]
-}
-}
 
 
-import_config = process_view_request('/api/configuration/import','POST',imp_data , user)
-import pdb;pdb.set_trace()
-names = ["token_normal_user_1","token_super_user_1"]
-for name in names:
-
-    import_config = process_view_request('/api/user?name=%s'%(name),'GET',{} , user).data['results'][0]
-    uuid = import_config['uuid']
-    import_config = process_view_request('/api/user/%s'%(uuid),'DELETE',{} , user)
 
 
 
--- scripts/sdk_sys.py
+++ scripts/sdk_sys.py
@@ -18,26 +18,15 @@ if __name__ == '__main__':
     api = ApiSession.get_session(args.controller_ip, args.user, args.password,
                                  tenant=args.tenant,api_version=args.version)
     
-
-    import ipdb;ipdb.set_trace()
-    data = {'name':'abcd111'}
-    post_data = api.post('pool',data=data)
-    uuid_abcd111 = json.loads(post_data.text)['uuid']
-    #data = {'model_name':'virtualservice','data':{'uuid':uuid}}
+    '''
     data = {'data':[
-            {'model_name':'pool', 'data':{'name':'abc222'}, 'method':'post'}, 
-            {'model_name':'pool', 'data':{'uuid':uuid_abcd111}, 'method':'delete'},
-            {'model_name':'poolgroup', 'data':{'name':'poolgroup111','members':[{'ratio':1, 'pool_ref':'/api/pool?name=abc222'}]}, 'method':'post'}
+        {'model_name':'pool', 'data':{'name':'pool-1'}, 'method':'post'}, 
             ]}
-    try:
-        post_data = api.post('macrostack', data=data)
-        if post_data.status_code == 201:
-            uuid_poolgroup111 = api.get_object_by_name('poolgroup', 'poolgroup111', 'admin')['uuid']
-            uuid_poolabc222 = api.get_object_by_name('pool', 'abc222', 'admin')['uuid']
-            api.delete('poolgroup/%s'%(uuid_poolgroup111))
-            api.delete('pool/%s'%(uuid_poolabc222))
-    finally:
-        api.delete('pool/%s'%(uuid_abcd111))
+    '''
+    data = {'data':[
+        {'model_name':'pool', 'data':{'name':'pool-1', 'tenant_ref':'/api/tenant/?name=t1'}, 'method':'delete'}, 
+            ]}
+    
+    post_data = api.post('macrostack', data=data)
 
 
-    #api.delete('sslkeyandcertificate/%s'%(obj['uuid']))
