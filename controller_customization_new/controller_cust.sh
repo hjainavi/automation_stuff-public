124e67cbaaa507cdece38470f44f7d2a42deb2b5
harsh jain <harshj@vmware.com>
2021-11-03 08:03:15 +0000

    controller_cust shorten

--- controller_customization_new/controller_cust.sh
+++ controller_customization_new/controller_cust.sh
@@ -7,7 +7,6 @@ set -x
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-sudo apt-get update
 
 
 if [ -d "/opt/avi/python" ]
