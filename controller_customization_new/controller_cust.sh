07f6c6cb0dda3c230ae061fd3fa7769cf4358427
harsh jain <harshj@vmware.com>
2023-03-16 07:18:14 +0000

    install and config files separate

--- controller_customization_new/controller_cust.sh
+++ controller_customization_new/controller_cust.sh
@@ -42,44 +42,6 @@ cp -v ./git_show_branch_cmd/git-prompt.sh ~/.config/git-prompt.sh
 
 #release=$(lsb_release -a 2>&1)
 #if [[ $release == *"focal"* ]]
-if false
-then
-    #mv /etc/apt/sources.list /etc/apt/sources_1.list_1
-    #cp -v ./other_files/sources.list /etc/apt/sources.list
-    sudo apt-get update;apt-get install aria2 ranger git mosh -y
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install tmux
-    #mv /etc/apt/sources_1.list_1 /etc/apt/sources.list
-
-else
-    echo "==================git config and bashrc done"
-    export DEBIAN_FRONTEND=noninteractive
-    sudo add-apt-repository ppa:pi-rho/dev -y
-    echo "==================add-apt done"
-
-    sudo apt-get update;apt-get install aria2 ranger git mosh -y
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install tmux
-
-    echo "==================apt-get done"
-fi
-    
-
-sudo pip3 install flake8
-sudo pip3 install ipdb
-echo "===============pip install flake8"
-git config --global --bool http.sslVerify false
-git config --global push.default current
-git config --global credential.helper "cache --timeout=3600"
-git config --global core.editor vim
-git config --global --bool core.autocrlf false
-git config --global --bool flake8.strict true
-echo "===============git config flake8.strict true"
-
-#if grep -q "alias ranger" "$HOME/.bashrc"; then
-#    echo "already changed other bashrc"
-#else
-#    cat ./other_bashrc_cust >> ~/.bashrc
-#fi
-
 
 cp -r ./other_files/tmux-resurrect ~/.config/tmux-resurrect
 cp -r ./other_files/tmux-continuum ~/.config/tmux-continuum
@@ -95,40 +57,5 @@ cp ./other_files/import1.py ~/import1.py
 
 echo "==================other_files done"
 
-vim +PluginInstall +qall
-echo "===================vim plugininstall done"
-
-#sed -i 's/X11Forwarding no/X11Forwarding yes/g' /etc/ssh/sshd_config
-#service ssh restart
-#echo "==================X11 done"
-
-if [ -d "/opt/avi/python" ];
-then
-    echo "creating a git repository of /opt/avi/python"
-cat >/opt/avi/python/.gitignore <<EOL
-*.pyo
-*.pyc
-*.swo
-*.swp
-EOL
-
-    cd /opt/avi/python
-    git config --global user.email "you@example.com"
-    git config --global user.name "Your Name"
-    git config --global pull.rebase false
-    git init
-    git add -A > /dev/null 2>&1
-    git commit -m "first commit" > /dev/null 2>&1
-    # flake8 --install-hook git
-    echo "===================git init done /opt/avi/python"
-else
-    echo "Not a controller VM setting up git config"
-    # run python script to install hook to run flake8 --install-hook git in repo
-    ./install-hook.py
-    git config --global user.email "harshj@vmware.com"
-    git config --global user.name "harsh jain"
-    git config --global pull.rebase false
-    echo "setting git global config user.email harshj@vmware.com"
-fi
-
+./controller_cust_install_only.sh
 
