9d6ad61cc5d542f8ce9d84b6fe30e86ab875351c
harsh jain <harshj@vmware.com>
2023-03-26 12:22:23 +0000

    only one logfile

--- scripts/git_fetch_cron_job.py
+++ scripts/git_fetch_cron_job.py
@@ -17,73 +17,65 @@ all_branches = sorted(all_branches, reverse=True)
 if os.path.isdir("/home/aviuser/workspace/avi-dev"):
     CWD = "/home/aviuser/workspace/avi-dev"
     FILE_PATH = "/home/aviuser/logfile_git_fetch_cron.txt"
-    FILE_PATH_ERROR = "/home/aviuser/logfile_git_fetch_cron_error.txt"
+FF = []
 
-def fetch_branch(CWD_c,branch,file_path,file_path_error, lock, remote_fetch=False):
-    ff = []
-    ffe = []
+def fetch_branch(CWD_c,branch, remote_fetch=False):
     try:
         start = '******** ' + str(datetime.datetime.now()) + '  ********\n'
-        ff.append(start)
-        ff.append("current dir: %s\n"%(CWD_c))
+        FF.append(start)
+        FF.append("current dir: %s\n"%(CWD_c))
         if remote_fetch:
             command = "git pull -p origin %s"%(branch)
         else:
             command = "git fetch -v -p -P origin %s:%s"%(branch,branch)
-        ff.append(command+"\n")
-        #subprocess.run(shlex.split(command), stdout=ff, stderr=ff, cwd=CWD, check=True, timeout=120)
+        FF.append(command+"\n")
+        #subprocess.run(shlex.split(command), stdout=FF, stderr=FF, cwd=CWD, check=True, timeout=120)
         result = subprocess.run(shlex.split(command), capture_output=True, text=True, cwd=CWD_c, check=True, timeout=120)
-        ff.append(str(result.stdout))
-        ff.append(str(result.stderr))
-        ff.append("\n")
+        FF.append(str(result.stdout))
+        FF.append(str(result.stderr))
+        FF.append("\n")
     except Exception as e:
         #raise
         #import ipdb;ipdb.set_trace()
-        ffe.append(start)
-        ffe.append(command+"\n")
-        ffe.append("ERROR !!!  ")
+        FF.append("ERROR !!! start ---- ")
+        FF.append(start)
+        FF.append(command+"\n")
         if not hasattr(e,"stderr"):
-            ffe.append(str(e)+"\n")
+            FF.append(str(e)+"\n")
             if "refusing to fetch into branch" in str(e):
                 cwd_c = "/home/aviuser/workspace/avi-dev"
                 if "avi-dev" in str(e):
                     cwd_c = "/home/aviuser/workspace/avi-dev"
                 if "dev-1" in str(e):
                     cwd_c = "/home/aviuser/workspace/dev-1"
-                fetch_branch(cwd_c,branch,file_path,file_path_error, lock, True)
+                fetch_branch(cwd_c,branch, True)
             if "bad object" in str(e) and "tags" in str(e):
                 tag = str(e).split("\n")[0].split("/")[-1]
                 command_1 = "git tag -d %s"%(tag)
                 subprocess.run(shlex.split(command_1), capture_output=True, text=True, cwd=CWD_c, check=True, timeout=120)
-                fetch_branch(CWD_c,branch,file_path,file_path_error, lock, False)
+                fetch_branch(CWD_c,branch, False)
         if hasattr(e,"stderr"):
-            ffe.append(str(e.stderr))
+            FF.append(str(e.stderr))
             
             if "refusing to fetch into branch" in str(e.stderr):
-                cwd_c = "/home/aviuser/workspace/avi-dev"
+                cwd_c = False
                 if "avi-dev" in str(e.stderr):
                     cwd_c = "/home/aviuser/workspace/avi-dev"
                 if "dev-1" in str(e.stderr):
                     cwd_c = "/home/aviuser/workspace/dev-1"
-                fetch_branch(cwd_c,branch,file_path,file_path_error, lock, True)
+                if "dev-2" in str(e.stderr):
+                    cwd_c = "/home/aviuser/workspace/dev-2"
+                if cwd_c:
+                    fetch_branch(cwd_c,branch, True)
             if "bad object" in str(e.stderr) and "tags" in str(e.stderr):
                 tag = str(e.stderr).split("\n")[0].split("/")[-1]
                 command_1 = "git tag -d %s"%(tag)
                 subprocess.run(shlex.split(command_1), capture_output=True, text=True, cwd=CWD_c, check=True, timeout=120)
-                fetch_branch(CWD_c,branch,file_path,file_path_error, lock, False)
-        #ffe.append(str(result.stdout))
-        #ffe.append(str(result.stderr))
-        ffe.append("\n")
-    if not ffe:
-        with lock:
-            with open(file_path, "a") as f:
-                for i in ff:
-                    f.write(i)
-    else:
-        with lock:
-            with open(file_path_error, "a") as f:
-                for i in ffe:
-                    f.write(i) 
+                fetch_branch(CWD_c,branch, False)
+        #FF.append(str(result.stdout))
+        #FF.append(str(result.stderr))
+        FF.append("Error End !!! ---- \n")
+    
 
 lock = threading.Lock()
 fetched_branches = []
@@ -98,7 +90,16 @@ for branch in all_branches:
 fetched_branches = ["eng","21.1.7"] + fetched_branches
 for branch in fetched_branches:
     print(branch)
-    fetch_branch(CWD, branch, FILE_PATH,FILE_PATH_ERROR, lock)
+    with lock:
+        with open(FILE_PATH, "a") as f:
+            f.write("\n\nWorking on branch :: %s\n"%(branch))
+    fetch_branch(CWD, branch)
+    with lock:
+        with open(FILE_PATH, "a") as f:
+            for i in FF:
+                f.write(i)
+            
+    FF = []
 
 
 # crontab -e has : 0 */8 * * * /usr/bin/python3 /home/aviuser/automation_stuff/scripts/git_fetch_cron_job.py
