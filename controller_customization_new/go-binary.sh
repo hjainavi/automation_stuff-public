2809dc300ad4f39e626a874341b481d04a6b7473
harsh jain <harsh.jain@broadcom.com>
2025-05-15 10:16:21 +0000

    allow to change apiserver and octavius

--- controller_customization_new/go-binary.sh
+++ controller_customization_new/go-binary.sh
@@ -3,6 +3,16 @@
 chmod +x ./_go-binary.py
 
 if [ "$1" = "change" ]; then
+    if [ -f "/home/admin/go-controller" ]; then
+        echo "/home/admin/go-controller file exists."
+        mv /home/admin/go-controller /opt/avi/bin/go-controller-new
+    else
+        echo "/home/admin/go-controller file does not exist."
+        if [ -z "/opt/avi/bin/go-controller-new" ]; then
+            echo "Error : /opt/avi/bin/go-controller-new file does not exist."
+            return 1
+        fi
+    fi
     mv /home/admin/go-controller /opt/avi/bin/go-controller-new
     if [ "$2" = "apiserver" ];then
         ./_go-binary.py change apiserver
