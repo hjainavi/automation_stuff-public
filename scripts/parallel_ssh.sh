238ac1081bc8f480dcde19df6ca0433ba47f7880
harsh jain <harsh.jain@broadcom.com>
2025-05-22 17:49:19 +0000

    added steps

--- scripts/parallel_ssh.sh
+++ scripts/parallel_ssh.sh
@@ -1,6 +1,7 @@
 # ssh-copy-id before doing below
 # split file --> split -n 10 ../debuglogs.20240927-144416_26f6f713.tar.gz 27.
 # ls | grep 27 | xargs -n 1 -P 10 ~/parallel_ssh.sh $(pwd) "aviuser@10.80.30.175:~/testing/AV-219088/"
+# rm 27.*
 # join files ---> cat 27.?? > debuglogs.20240927-144416_26f6f713.tar.gz
 
 rsync -avz --progress $1/$3 $2
