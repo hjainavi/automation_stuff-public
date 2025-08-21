f57362445589ebc0645ffea253f6bc080b90e486
harsh jain <harsh.jain@broadcom.com>
2025-08-21 06:35:01 +0000

    sdk_pool changes

--- scripts/sdk_pool.py
+++ scripts/sdk_pool.py
@@ -1,6 +1,5 @@
 from avi.sdk.avi_api import ApiSession
 import argparse, json
-from concurrent.futures import ThreadPoolExecutor
 
 
 if __name__ == '__main__':
@@ -18,18 +17,18 @@ if __name__ == '__main__':
     args = parser.parse_args()
     api = ApiSession.get_session(args.controller_ip, args.user, args.password,
                                  tenant=args.tenant,api_version=args.version)
-    '''
-    post_data = {
-            'name':'test123',
-            'servers':[{'ip':{'addr':'0.0.0.0', 'type':'V4'} ,'enabled':True, 'hostname':'abc.def', 'resolve_server_by_dns':True}]
-        }
     name = "test_"
     for i in range(201):
         name_new = name+str(i)
         post_data = {"name":name_new}
-        post_resp = api.post('pool', data=post_data)
-        print ("post = %s "%post_resp.status_code)
-    '''
+        #post_resp = api.post('pool', data=post_data)
+        #print ("post = %s "%post_resp.status_code)
 
-    get_resp = api.get('systemconfiguration')
-    print(json.dumps(get_resp.data,indent=4)
+    get_resp = api.get('pool?fields=name&page_size=-1')
+    #get_resp = api.get('pool?fields=name&page=2')
+    print(json.dumps(json.loads(get_resp.content),indent=4))
+
+    for uuid in json.loads(get_resp.content):
+        #delete_resp = api.delete('pool', uuid)
+        #print ("delete = %s "%delete_resp.status_code)
+        pass
