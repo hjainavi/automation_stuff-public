c27a2e45cee83a11fd6334b6391c5b4dd52cf268
harsh jain <harsh.jain@broadcom.com>
2025-08-21 06:35:39 +0000

    sdk_pool changes

--- controller_customization_new/other_files/sdk_pool.py
+++ controller_customization_new/other_files/sdk_pool.py
@@ -1,5 +1,6 @@
 from avi.sdk.avi_api import ApiSession
-import argparse, urllib, json
+import argparse, json
+
 
 if __name__ == '__main__':
 
@@ -11,20 +12,23 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--tenant', help='tenant name',
                         default='admin')
     parser.add_argument('-c', '--controller_ip', help='controller ip')
-    parser.add_argument('-v', '--version', help='version')
+    parser.add_argument('-v', '--version', help='controller ip')
+
     args = parser.parse_args()
     api = ApiSession.get_session(args.controller_ip, args.user, args.password,
                                  tenant=args.tenant,api_version=args.version)
-    
-    import ipdb; ipdb.set_trace()
-    #name = u"pool77AlmaWeb_St\xf6rung".encode('utf-8')
-    #name_encoded = urllib.quote(name)
-    #pool_data = {'name':name}
-    #post_data = api.post('pool', data = pool_data)
-    
-    get_name_data = api.get('pool?name=pool_scale_1')
-    json_get_name_data = json.loads(get_name_data.text)['results'][0]
-    uuid = json_get_name_data['uuid']
-    put_res = api.put('pool/%s'%(uuid), data = json_get_name_data)
-    pass
-    #api.delete('sslkeyandcertificate/%s'%(obj['uuid']))    
+    name = "test_"
+    for i in range(201):
+        name_new = name+str(i)
+        post_data = {"name":name_new}
+        #post_resp = api.post('pool', data=post_data)
+        #print ("post = %s "%post_resp.status_code)
+
+    get_resp = api.get('pool?fields=name&page_size=-1')
+    #get_resp = api.get('pool?fields=name&page=2')
+    print(json.dumps(json.loads(get_resp.content),indent=4))
+
+    for uuid in json.loads(get_resp.content):
+        #delete_resp = api.delete('pool', uuid)
+        #print ("delete = %s "%delete_resp.status_code)
+        pass
